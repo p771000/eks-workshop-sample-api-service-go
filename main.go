8b536c707c0a46b53f97c1fc245931fd166c9cd7
p771000 <85852557+p771000@users.noreply.github.com>
2021-06-20 21:43:09 +1000

    Update main.go
    new message

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello from github - codebuild - codepipeline to this Kubernetes World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
